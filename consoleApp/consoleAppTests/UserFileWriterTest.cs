e85a6d38524c7f67af43b99a65f92c518b9ac22f
Vilde Vevatne <v-viveva@microsoft.com>
2018-08-07 10:42:15 -0700

    Add testfile, assert content is what you think it is

--- consoleApp/consoleAppTests/UserFileWriterTest.cs
+++ consoleApp/consoleAppTests/UserFileWriterTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace consoleAppTests
@@ -6,9 +7,18 @@ namespace consoleAppTests
     [TestClass]
     public class UserFileWriterTest
     {
+        public static TestContext TestContext { get; set; }
         [TestMethod]
+        [DeploymentItem("TestFile.txt")]
         public void TestMethod1()
         {
+            var directory = TestContext.DeploymentDirectory;
+            var pathToFile = Path.Combine(directory, "TestFile.txt");
+
+            var content = File.ReadAllText(pathToFile);
+            Assert.AreEqual("This is a test", content);
+
+        
         }
     }
 }
