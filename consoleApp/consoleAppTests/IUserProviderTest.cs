259142a7076e34dfe576c648d005b551ef2a546b
Vilde Vevatne <v-viveva@microsoft.com>
2018-08-07 11:58:37 -0700

    Move tesfile to properties folder

---
+++ consoleApp/consoleAppTests/IUserProviderTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace consoleAppTests
+{
+    [TestClass]
+    public class IUserProviderTest
+    {
+        [TestMethod]
+        public void TestGetUsers()
+        {
+            var mockDataProvider = new Mock<consoleApp.IUserProvider>(MockBehavior.Strict);
+            mockDataProvider.Setup(x => x.GetUsers()).Returns()
+        }
+    }
+}
