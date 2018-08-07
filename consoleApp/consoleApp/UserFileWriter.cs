9bea8ffc4034e846c37582a5e585031ea4d8be11
Vilde Vevatne <v-viveva@microsoft.com>
2018-08-07 10:33:53 -0700

    Add userfilewriter class

---
+++ consoleApp/consoleApp/UserFileWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleApp
+{
+    public class UserFileWriter
+    {
+        public void WriteUsers(IEnumerable<User> users, string filePath)
+        {
+            using (var sw = new StreamWriter(filePath))
+            {
+                foreach(var user in users)
+                {
+                    sw.WriteLine(user.FirstName + " " + user.LastName);
+                }
+            }
+        }
+    }
+}
---
+++ consoleApp/consoleAppTests/UserFileWriterTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace consoleAppTests
+{
+    [TestClass]
+    public class UserFileWriterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
